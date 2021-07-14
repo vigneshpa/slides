34e008e536900ff3a8c2a37d1c00d2bf5049889a
vignesh <vignesh-p@outlook.com>
2021-07-14 17:42:11 +0530

    common thank you page

--- src/presentations/test.js
+++ src/presentations/test.js
@@ -1,6 +1,7 @@
+import {addFinal} from "../common.js";
 import Reveal from 'reveal.js';
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/black.css";
-import "../common.js"
 let deck = new Reveal()
 deck.initialize();
+addFinal("test");
