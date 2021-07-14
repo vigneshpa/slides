29d4303104fbec4712e2f716ab129b98c23ba8b1
vignesh <vignesh-p@outlook.com>
2021-07-14 22:20:08 +0530

    creating pdf using puppeter core

--- src/print.js
+++ src/print.js
@@ -1,23 +1,37 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const resolve = require('path').resolve;
 const slide = process.env.SLIDE || 'test';
-const page = resolve(__dirname, `../${slide}.html`);
+const pageURL = 'file://' + resolve(__dirname, `../${slide}.html?print-pdf`);
 const pdf = resolve(__dirname, `../pdf/${slide}.pdf`);
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false
+    executablePath:"google-chrome-stable",
+    headless: true
   });
   const page = await browser.newPage();
-  await page.goto(page, {
-    waitUntil: 'networkidle2'
-  })
+
+  // await page.setViewport({ width: 1920, height: 1080 });
+
+  await page.goto(pageURL, {
+    waitUntil: 'networkidle0'
+  });
+
+  await WaitMs(5000);
 
   await page.pdf({
-    format: 'A5',
+    margin: 0,
+    displayHeaderFooter:false,
+    landscape: true,
+    printBackground: true,
+    omitBackground: false,
     path: pdf
-  })
+  });
 
   // close the browser
   await browser.close();
-})()
+})();
+
+function WaitMs(ms){
+  return new Promise(resolve=>setTimeout(e=>resolve(), ms));
+}
