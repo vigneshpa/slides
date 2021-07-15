93580a1fc281daab28f0d7e6bfdec0e7ee222b7a
vignesh <vignesh-p@outlook.com>
2021-07-15 18:46:47 +0530

    improving

--- src/print.js
+++ src/print.js
@@ -7,19 +7,24 @@ const pdf = resolve(__dirname, `../pdf/${slide}.pdf`);
 const product = "chrome" || "firefox";
 
 (async () => {
+  console.log("Creating an browser instance");
   const browser = await puppeteer.launch({
     executablePath:product,
     product,
     headless: true
   });
+
+
+  console.log("Creating a new tab");
   const page = await browser.newPage();
 
+  console.log("Navigating to the presentation");
   await page.goto(pageURL, {
     waitUntil: 'networkidle0'
   });
-
   await WaitMs(2000);
 
+  console.log("Printing the pdf");
   await page.pdf({
     width:999,
     height:728,
@@ -30,7 +35,10 @@ const product = "chrome" || "firefox";
     displayHeaderFooter:false,
     margin:0
   });
+  
+  console.log("Closing browser");
   await browser.close();
+  console.log("All done!");
 })();
 
 function WaitMs(ms){
