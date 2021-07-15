6ccd8a20b4fa6ab6facd18dd7f5cf83c80540526
vignesh <vignesh-p@outlook.com>
2021-07-15 20:03:51 +0530

    adding zoom

--- src/presentations/test/test.js
+++ src/presentations/test/test.js
@@ -2,7 +2,8 @@ import Reveal from 'reveal.js';
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/black.css";
 
-import revealMath from "reveal.js/plugin/math/math.esm"
+import revealMath from "reveal.js/plugin/math/math.esm";
+import revealZoom from "reveal.js/plugin/zoom/zoom.esm";
 
 let deck = new Reveal({
   math: {
@@ -11,6 +12,6 @@ let deck = new Reveal({
     // pass other options into `MathJax.Hub.Config()`
     TeX: { Macros: { RR: "{\\bf R}" } }
   },
-  plugins:[revealMath]
+  plugins:[revealMath, revealZoom]
 })
 deck.initialize();
