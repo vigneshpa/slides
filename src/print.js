b5c855f36ade006a97d55dd903faa9ac5326f8a3
vignesh <vignesh-p@outlook.com>
2021-07-15 16:14:23 +0530

    perfect pdf setup

--- src/print.js
+++ src/print.js
@@ -4,34 +4,35 @@ const slide = process.env.SLIDE || 'test';
 const pageURL = 'file://' + resolve(__dirname, `../${slide}.html?print-pdf`);
 const pdf = resolve(__dirname, `../pdf/${slide}.pdf`);
 
+const product = "chrome" || "firefox";
+
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath:"google-chrome-stable",
+    executablePath:product,
+    product,
     headless: true
   });
   const page = await browser.newPage();
-
-  // await page.setViewport({ width: 1920, height: 1080 });
 
   await page.goto(pageURL, {
     waitUntil: 'networkidle0'
   });
 
-  await WaitMs(5000);
+  await WaitMs(2000);
 
   await page.pdf({
-    margin: 0,
-    displayHeaderFooter:false,
+    width:999,
+    height:728,
     landscape: true,
     printBackground: true,
-    omitBackground: false,
-    path: pdf
+    omitBackground:false,
+    path: pdf,
+    displayHeaderFooter:false,
+    margin:0
   });
-
-  // close the browser
   await browser.close();
 })();
 
 function WaitMs(ms){
-  return new Promise(resolve=>setTimeout(e=>resolve(), ms));
+  return new Promise(resolve=>setTimeout(resolve, ms));
 }
