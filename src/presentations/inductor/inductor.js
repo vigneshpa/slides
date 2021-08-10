d4c4eb421a9ef4db129b96658babd8dd2acdbad3
vignesh <vignesh-p@outlook.com>
2021-08-10 13:37:40 +0530

    changing styles and creating sine wave generator

--- src/presentations/inductor/inductor.js
+++ src/presentations/inductor/inductor.js
@@ -1,18 +1,2 @@
-import Reveal from 'reveal.js';
-import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/black.css';
-
-import revealMath from 'reveal.js/plugin/math/math.esm';
-import revealZoom from 'reveal.js/plugin/zoom/zoom.esm';
+import './inductor.css';
 import './inductor.ts';
-
-let deck = new Reveal({
-  math: {
-    mathjax: 'https://cdn.jsdelivr.net/gh/mathjax/mathjax@2.7.8/MathJax.js',
-    config: 'TeX-AMS_HTML-full',
-    // pass other options into `MathJax.Hub.Config()`
-    TeX: { Macros: { RR: '{\\bf R}' } },
-  },
-  plugins: [revealMath, revealZoom],
-});
-deck.initialize();
--- src/presentations/inductor/inductor.ts
+++ src/presentations/inductor/inductor.ts
@@ -1,11 +1,14 @@
-const angVelCurrentRatio = 1;
-const inductance = 10;
+const inductance = 10; // in Henries(H)
+const resistance = 5;  // in Ohms(Ω)
+
+const angularVelocityCurrentRatio = 1;
 
 let voltage = 0;
 let rps = 0;
 
+const voltageInp = document.getElementsByName('voltage')[0] as HTMLInputElement;
 const volEl = document.getElementById('voltageValue')!;
-(<any>window).onVoltage = (val: string) => {
+window.onVoltage = (val: string) => {
   voltage = +val;
   volEl.innerText = (+val).toFixed(1);
 };
@@ -14,32 +17,67 @@ const curEl = document.getElementById('currentValue')!;
 const wheelEl = document.getElementById('flywheel')!;
 let wheelR: number = 0;
 
+
+// Current integral for integration
 let currentIntegral: number = 0;
 
-let loadtype: 'Resistive' | 'Inductive' = 'Resistive';
-(<any>window).toogleResInd = () => (currentIntegral = 0) || (loadtype = loadtype === 'Inductive' ? 'Resistive' : 'Inductive');
+// Sine function generator
+let generatorStartTS:number|null = null;
+let generateSine: boolean = false;
+window.onGenerateSine = checked => {generatorStartTS = null;currentIntegral=0;rps=0; generateSine = !!checked};
 
 // Resistor function
-const integrateResistorCurrent = (delta: number) => voltage / 10;
+const integrateResistorCurrent = (delta: number) => voltage / resistance;
 
 // Inductor function
-const integrateInductorCurrent = (delta: number) => (currentIntegral += (delta * voltage) / inductance); // as V=-L(di/dt)
+const integrateInductorCurrent = (delta: number) => currentIntegral += (delta * voltage) / inductance; // as V=-L(di/dt)
+
+// ResInd function
+const integrateResIndCurrent = (delta: number) => (currentIntegral += (delta * (voltage - (currentIntegral * resistance))) / inductance); // as V=-L(di/dt)+iR
+
+let integrateCurrent = integrateResistorCurrent;
+
+window.onCircuitType = loadt => {
+  currentIntegral = 0;
+  if(generateSine)window.onGenerateSine(true);
+  switch (loadt) {
+    case 'Resistive':
+      integrateCurrent = integrateResistorCurrent;
+      break;
+    case 'Inductive':
+      integrateCurrent = integrateInductorCurrent;
+      break;
+    case 'ResInd':
+      integrateCurrent = integrateResIndCurrent;
+      break;
+    default:
+      integrateCurrent = integrateResistorCurrent;
+  }
+}
 
 let previousTS: number = 0;
 const step: FrameRequestCallback = timeStamp => {
   const delta = (timeStamp - previousTS) / 1000;
 
-  const current = loadtype === 'Resistive' ? integrateResistorCurrent(delta) : integrateInductorCurrent(delta);
+  const current = integrateCurrent(delta);
 
-  rps = angVelCurrentRatio * current;
+  rps = angularVelocityCurrentRatio * current;
 
   wheelR += rps * delta * 360;
   wheelEl.style.transform = `rotate(${wheelR}deg)`;
 
   // --
   curEl.innerText = current.toFixed(1);
+  if(!generatorStartTS)generatorStartTS = timeStamp;
+  if (generateSine)
+    (<any>volEl).innerText = ((<any>voltageInp).value = voltage = 10 * Math.sin((timeStamp-generatorStartTS) / 1000)).toFixed(1);
   previousTS = timeStamp;
   window.requestAnimationFrame(step);
-  console.log({ delta });
 };
 window.requestAnimationFrame(step);
+
+interface Window {
+  onVoltage: (val: string) => void;
+  onCircuitType: (loadt: 'Resistive' | 'Inductive' | 'ResInd') => void;
+  onGenerateSine: (checked: boolean) => void;
+}
